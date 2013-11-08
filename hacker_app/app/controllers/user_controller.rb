268c97249d2ab452ca486e7a627935c7339b726f
Dan Crews <dancrews@me.com>
2013-11-07 20:28:51 -0500

    Edited User controller.

--- hacker_app/app/controllers/user_controller.rb
+++ hacker_app/app/controllers/user_controller.rb
@@ -1,2 +1,31 @@
 class UserController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  # process the signup form
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Slacker News." 
+      redirect_to root_url
+    else
+      redirect_to root_url, notice: "Something went wrong. Try again!"
+    end
+  end
+
+  def destroy
+    user_id = current_user.id
+    user = User.find(user_id)
+    user.destroy
+    redirect_to root_url, notice: "Account closed."
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password)
+  end
+
 end
